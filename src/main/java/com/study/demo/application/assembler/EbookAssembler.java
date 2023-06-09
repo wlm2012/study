3adeb8ce689d70d5f8c8ba1240c88d3892db47a3
wlm2012 <541253785@qq.com>
2023-06-09 21:49:04 +0800

    inti

--- src/main/java/com/study/demo/application/assembler/EbookAssembler.java
+++ src/main/java/com/study/demo/application/assembler/EbookAssembler.java
@@ -3,10 +3,7 @@ package com.study.demo.application.assembler;
 import com.study.demo.application.dto.request.EbookAddRequest;
 import com.study.demo.domain.model.EbookEntity;
 import com.study.demo.infrastructure.common.annotation.IdIgnore;
-import org.mapstruct.MapMapping;
 import org.mapstruct.Mapper;
-
-import java.lang.annotation.Target;
 
 @Mapper(componentModel = "spring")
 public interface EbookAssembler {
