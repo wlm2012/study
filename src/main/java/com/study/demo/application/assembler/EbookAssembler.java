6a202c27bd49c5f251d05d369f88528855580129
wlm2012 <541253785@qq.com>
2023-06-09 21:47:09 +0800

    inti

--- src/main/java/com/study/demo/application/assembler/EbookAssembler.java
+++ src/main/java/com/study/demo/application/assembler/EbookAssembler.java
@@ -2,10 +2,15 @@ package com.study.demo.application.assembler;
 
 import com.study.demo.application.dto.request.EbookAddRequest;
 import com.study.demo.domain.model.EbookEntity;
+import com.study.demo.infrastructure.common.annotation.IdIgnore;
+import org.mapstruct.MapMapping;
 import org.mapstruct.Mapper;
+
+import java.lang.annotation.Target;
 
 @Mapper(componentModel = "spring")
 public interface EbookAssembler {
 
+    @IdIgnore
     EbookEntity toEntity(EbookAddRequest ebookAddRequest);
 }
---
+++ src/main/java/com/study/demo/infrastructure/common/annotation/IdIgnore.java
@@ -0,0 +1,14 @@
+package com.study.demo.infrastructure.common.annotation;
+
+import org.mapstruct.Mapping;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.CLASS)
+@Mapping(target = "id", ignore = true)
+public @interface IdIgnore {
+}
