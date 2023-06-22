222783d6ba020591a4c283ae4a745a590a2ac408
wlm2012 <541253785@qq.com>
2023-06-22 19:16:04 +0800

    update

--- src/main/java/com/study/demo/adapter/controller/EbookController.java
+++ src/main/java/com/study/demo/adapter/controller/EbookController.java
@@ -6,6 +6,7 @@ import com.study.demo.application.dto.request.IdCmd;
 import com.study.demo.application.dto.response.EbookDTO;
 import com.study.demo.application.service.EbookServiceImpl;
 import com.study.demo.domain.model.EbookEntity;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.RequiredArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
@@ -27,13 +28,13 @@ public class EbookController {
     }
 
     @GetMapping("/{id}")
-    public EbookDTO findById(@PathVariable("id") Long id) {
+    public EbookDTO findById(@PathVariable("id") @NotEmpty(message = "id不能为空") Long id) {
         EbookEntity ebookEntity = ebookService.findById(id);
         return ebookAssembler.toDTO(ebookEntity);
     }
 
     @PostMapping("/deleteById")
-    public void deleteById(@RequestBody IdCmd idCmd) {
+    public void deleteById(@RequestBody @Validated IdCmd idCmd) {
         ebookService.deleteById(idCmd.getId());
     }
 
--- src/main/java/com/study/demo/application/dto/request/IdCmd.java
+++ src/main/java/com/study/demo/application/dto/request/IdCmd.java
@@ -1,8 +1,11 @@
 package com.study.demo.application.dto.request;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 @Data
 public class IdCmd {
+
+    @NotEmpty(message = "id不能为空")
     private Long id;
 }
